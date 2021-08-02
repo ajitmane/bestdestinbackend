58943592f974d91e754e78b68e6077cf5d09e301
cmaneaj <cmaneaj@jci.com>
2021-08-02 18:05:38 +0530

    test commit

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/users', apiRouter);
+//app.use('/api/users', apiRouter);
+
+app.use('/api/users', (req, res) =>{
+    res.json({"message":"Fetched successfully"});
+    res.sendStatus(200);
+});
 
 app.listen(process.env.PORT || '3000', () =>{
     console.log(`Server is running on port :${process.env.PORT || '3000'}`)
